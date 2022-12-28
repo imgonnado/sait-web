d1e36f5c31de83f0d2d438c4341111178deb1f0e
seambark <run128@naver.com>
2022-12-28 23:54:39 +0900

    #1 :lipstick: 체크박스 컴포넌트 작성

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./Checkbox.module.scss";
+
+interface CheckboxProps {
+  children: React.ReactNode;
+  id?: string;
+  name?: string;
+  checkboxType?: "toggle";
+  textColor?: "on";
+  disabled?: boolean;
+}
+
+function Checkbox({
+  children,
+  id,
+  name,
+  checkboxType,
+  textColor,
+  disabled,
+}: CheckboxProps) {
+  return (
+    <>
+      <input
+        type="checkbox"
+        className={clsx(
+          styles.checkbox,
+          checkboxType && styles[`${checkboxType}`]
+        )}
+        id={id}
+        name={name}
+        disabled={disabled}
+      />
+      <label
+        className={clsx(
+          styles.checkboxLabel,
+          textColor && styles[`${textColor}`]
+        )}
+        htmlFor={id}
+      >
+        {children}
+      </label>
+    </>
+  );
+}
+
+export default Checkbox;
