253fcc7d2bee513275bc1f2833959ba9849d419f
BonhaengLee <bon952014@ajou.ac.kr>
2023-01-08 00:31:53 +0900

    #18 :lipstick: setting page

---
+++ pages/setting/index.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactElement } from "react";
+
+import CommonLayout from "../../src/components/Layout/CommonLayout";
+import Text from "../../src/components/Typography/Text";
+
+import styles from "./Setting.module.scss";
+
+const SETTING_LIST = [
+  {
+    title: "알림 설정",
+    link: "/setting/notification",
+  },
+  {
+    title: "1:1 문의",
+    link: "/setting/inquiry",
+  },
+  {
+    title: "개인정보 보호 지침",
+    link: "/setting/privacy",
+  },
+  {
+    title: "이용약관동의",
+    link: "/setting/terms",
+  },
+  {
+    title: "회원탈퇴",
+    link: "/setting/withdrawal",
+  },
+  {
+    title: "로그아웃",
+    link: "/setting/logout",
+  },
+];
+
+function Index() {
+  return (
+    <>
+      {SETTING_LIST.map(({ title, link }) => (
+        <Link href={link}>
+          <a className={styles.listItem}>
+            <Text>{title}</Text>
+            <Image
+              src="/asset/image/ico/ico_detail_arrow.svg"
+              alt="arrow"
+              layout="fixed"
+              width={16}
+              height={16}
+            />
+          </a>
+        </Link>
+      ))}
+    </>
+  );
+}
+
+export default Index;
+
+Index.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <CommonLayout header={false} mainClassName={styles.main}>
+      {page}
+    </CommonLayout>
+  );
+};
--- src/components/Layout/CommonLayout.tsx
+++ src/components/Layout/CommonLayout.tsx
@@ -3,20 +3,34 @@ import React, { ReactElement } from "react";
 
 import Header from "./Header/Header";
 import GNB from "./NavigationBar/GNB";
+import Top from "../Top/Top";
 
 import styles from "./CommonLayout.module.scss";
 
 interface CommonLayoutProps {
   children: React.ReactNode;
+  header?: boolean;
+  top?: boolean;
+  mainClassName?: string;
+  mainStyles?: React.CSSProperties;
 }
 
 export default function CommonLayout({
   children,
+  header = true,
+  top = true,
+  mainClassName,
+  mainStyles,
 }: CommonLayoutProps): ReactElement {
   return (
     <div className={styles.wrap}>
-      <Header />
-      <main id="main_content" className={clsx(styles.content, styles.main)}>
+      {header && <Header />}
+      {top && <Top />}
+      <main
+        id="main_content"
+        className={clsx(styles.content, styles.main, mainClassName)}
+        style={mainStyles}
+      >
         {children}
       </main>
       <GNB />
